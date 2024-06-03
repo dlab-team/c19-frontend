c62c2924acb9a7b4a8618fa450a8172f2fd714b6
anashei <santinoazarola@gmail.com>
2024-06-03 15:31:22 -0300

    footer terminado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,69 +1,48 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Container from "react-bootstrap/Container";
 
 export const Footer = () => {
   return (
-    <footer>
-      <div className="footer-content">
-        <div className="footer-logo">
-          <Link href="https://desafiolatam.com/">
-            <Image
-              src={"/logo-adl-blanco-verde.png"}
-              alt={"Desafio Latam"}
-              width={200}
-              height={107}
-              className=""
-            >         
-            </Image>
-          </Link>
+    <footer className="row bg-black">
+      <div className="container col-lg">
+        <div className="h-50 py-2 d-flex align-items-center">
+        <Link href="https://desafiolatam.com/">
+        <Image 
+            src={"/logo-academia-ne.png"}
+            alt={"logo academia"}
+            width={140}
+            height={52}
+            className="">
+        </Image>
+        </Link>
         </div>
-        <Container>
-          <div className="row">
-            <div className="col-12 col-md-4 col-xl-4">
-              <div className="footer-partners">
-                <h3>Contacto General</h3>
-                <h5><i className="fa fa-whatsapp" aria-hidden="true"></i>+56 9 5117 7975</h5>
-                <h5><i className="fa fa-whatsapp" aria-hidden="true"></i>+52 1 55 4047 7251</h5>
-                <h4>Horario de Atencion WhatsApp:</h4>
-                <span>Lunes a Viernes de 10:00 a 18:00</span>
-                <h4 className="mt-2">Contacto Admisión:</h4>
-                <Link className="mailto" href="mailto:inscripciones@desafiolatam.com">inscripciones@desafiolatam.com</Link>
-                <h4 className="mt-2">Contacto Estudiantes:</h4>
-                <Link className="mailto" href="mailto:ayuda@desafiolatam.com">ayuda@desafiolatam.com</Link>
-              </div>
-            </div>
-            <div className="col-12 col-md-4 col-xl-4">
-              <div className="footer-partners">
-                <h3>Carreras</h3>
-                <ul>
-                  <li><Link href="https://desafiolatam.com/full-stack-javascript-b/">Desarrollo Full Stack JavaScript</Link></li>
-                  <li><Link href="https://desafiolatam.com/diseño-ux-ui/">Diseño UX/UI</Link></li>
-                  <li><Link href="https://desafiolatam.com/data-science/">Data Science</Link></li>
-                  <li><Link href="https://desafiolatam.com/front-end-react/">Front End</Link></li>
-                  <li><Link href="https://desafiolatam.com/data-analytics/">Data Analytics</Link></li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-12 col-md-4 col-xl-4">
-              <div className="footer-partners">
-                <h3>Nuestra comunidad</h3>
-                <ul>
-                <li><Link href="https://desafiolatam.com/estudiantes/">Estudiantes</Link></li>
-                <li><Link href="https://desafiolatam.com/comunidad/">Comunidad</Link></li>
-                <div className="dropdown-divider"></div>
-                <li><Link href="https://blog.desafiolatam.com/">Blog</Link></li>
-                <li><Link href="https://desafiolatam.com/becas/">Becas</Link></li>
-                <li><Link href="https://desafiolatam.com/trabaja-con-nosotros/">Trabaja con Nosotros</Link></li>
-                <li><Link href="https://desafiolatam.com/equipo-docente/">Postula para ser docente</Link></li>
-                <li><Link href="https://desafiolatam.com/politica-de-calidad/">Políticas de Calidad</Link></li>
-                <li><Link href="https://desafiolatam.com/poltica-privacidad-de-datos/">Políticas de Privacidad y Protección de Datos</Link></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </Container>
+        <div className="h-50 py-2 d-flex flex-row align-items-end ">
+          <Link className="fa fa-facebook-square fs-5 px-2 text-secondary text-decoration-none" href="https://www.facebook.com/DesafioLatam/"></Link>
+          <Link className="fa fa-linkedin-square fs-5 px-2 text-secondary text-decoration-none" href="https://www.facebook.com/DesafioLatam/"></Link>
+          <Link className="fa fa-youtube-play fs-5 px-2 text-secondary text-decoration-none" href="https://www.facebook.com/DesafioLatam/"></Link>
+          <Link className="fa fa-instagram fs-5 px-2 text-secondary text-decoration-none" href="https://www.facebook.com/DesafioLatam/"></Link>
+        </div>
+      </div>
+      <div className="container col-lg row">
+        <div className="col-md-4 py-2 d-flex flex-column">
+          <p className="text-dark m-0 py-2">Topic</p>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+        </div>
+        <div className="col-md-4 py-2 d-flex flex-column">
+          <p className="text-dark m-0 py-2">Topic</p>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+        </div>
+        <div className="col-md-4 py-2 d-flex flex-column">
+          <p className="text-dark m-0 py-2">Topic</p>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+          <Link className="text-secondary text-decoration-none py-2"href="">Page</Link>
+        </div>
       </div>
     </footer>
   )
