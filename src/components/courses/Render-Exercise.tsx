d555c1e85760e269ad74b9f1ddefa1df87655204
Franco Ignacio Villca <ignafrancov@gmail.com>
2024-06-06 17:35:13 -0300

    fix const contenido

--- src/components/courses/Render-Exercise.tsx
+++ src/components/courses/Render-Exercise.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import {Container} from "react-bootstrap";
 
 interface Props{
-    contenidohtml:string;
-    contenidocss:string;
+    contenidoHtml:string;
+    contenidoCss:string;
 }
 
-export const Render = ({contenidohtml, contenidocss}:Props) =>{
-    const contenido = contenidohtml+contenidocss
+export const Render = ({contenidoHtml, contenidoCss}:Props) =>{
+    const contenido = `<!DOCTYPE html>
+        <html>
+        <head>
+            <style>${contenidoCss}</style>
+        </head>
+        <body>
+            ${contenidoHtml}
+        </body>
+        </html>`
     return(
         <Container>
         <div style={{height: "60vh", border:"1px solid black", boxShadow:"3px 3px 5px #444" ,borderRadius:"0 0 5px 5px"}}>
