aa4365b83f6cf94ad42a0a97d95fbc02aafcafd1
Daniel Machado <needforock@gmail.com>
2024-06-08 14:43:13 -0400

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
 
+const __dirname = new URL(".", import.meta.url).pathname;
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
+};
 export default nextConfig;
