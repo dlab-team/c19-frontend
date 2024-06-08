eff31f2d9268b9b0d9e0b1246a20659721ff7874
Daniel Machado <needforock@gmail.com>
2024-06-08 14:52:25 -0400

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"),
     };
     return config;
   },
