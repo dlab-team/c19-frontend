b4f524c1683621fd0927f05b3adcb1478aacf3de
Oscar Lozano <olozano1194@gmail.com>
2024-06-15 04:10:32 -0500

    desactivar btn Anterior y Siguiente cuando id está fuera de rango

--- src/components/common/Advance.tsx
+++ src/components/common/Advance.tsx
@@ -1,39 +1,53 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { Container } from "react-bootstrap";
 import { Discord } from "./Discord";
-
 
 interface Props {
   actualStep: number;
 }
 
 export const Advance = ({ actualStep }: Props) => {
+  const router = useRouter();
 
-    const [step, setStep] = useState<number>(actualStep);
-    const router = useRouter();
+  const handlepreviousStep = () => {
+    const previousStep = Math.max(actualStep -1, 1);
+    router.push(`/courses/html-css/${previousStep}`);
+  }
+
+  const handleNextStep = () => {
+    const nextStep = Math.min(actualStep + 1 , 4);
+    router.push(`/courses/html-css/${nextStep}`);
+  }
+
   return (
-    <>
+    <div className="mb-3">
       <Discord />
-      <Container className="d-flex justify-content-between px-1 mt-1">
+      <Container className="d-flex justify-content-between px-1 mt-3">
         <div className="d-flex justify-content-end px-2 ">
           <button
             className="bg_excercises px-4 py-2 next_button"
-            onClick={() => router.push(`/courses/html-css/${actualStep - 1}`)}
+            //onClick={() => router.push(`/courses/html-css/${actualStep - 1}`)}
+            onClick={handlepreviousStep}
+            disabled={actualStep <=1} //si el id es menor a uno entonces el btn, 
+                                      //se suspende
           >
             Anterior
           </button>
         </div>
-        <div
-          className="d-flex justify-content-end px-2 "
-          onClick={() => router.push(`/courses/html-css/${actualStep + 1}`)}
-        >
-          <button className="bg_excercises px-4 py-2 next_button">
+        <div className="d-flex justify-content-end px-2 ">
+          <button
+            className="bg_excercises px-4 py-2 next_button"
+            //onClick={() => router.push(`/courses/html-css/${actualStep + 1}`)}
+            onClick={handleNextStep}
+            disabled={actualStep >= 4} //si el id es mayor a 4 entonces el
+                                        //btn se suspende
+          >
             Siguiente
           </button>
         </div>
       </Container>
-    </>
+    </div>
   );
 };
