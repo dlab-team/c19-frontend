a949ca7b1d22232ddd15173a03c262500fbb4848
Daniel Machado <needforock@gmail.com>
2024-07-22 17:22:11 -0400

    fix: style IA

--- src/components/ia/IAMessage.tsx
+++ src/components/ia/IAMessage.tsx
@@ -1,24 +1,47 @@
 "use client";
-import type { Response } from "@/actions/problems-client-actions";
+
 import { getPhrase } from "@/helpers/random-phrases";
 import Image from "next/image";
-import React, { useState } from "react";
-import Spinner from "react-bootstrap/Spinner";
+import React, { useEffect, useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 import { motion } from "react-magic-motion";
 
 interface Props {
-  response: Response;
-  isLoading: boolean;
+  response: IAResponse;
+  showDiv: boolean;
+  setShowDiv: (arg: boolean) => void;
+  handleRequestIA: () => void;
 }
 
-export const IAMessage = ({ response, isLoading }: Props) => {
-  const [showDiv, setShowDiv] = useState(false);
+export interface IAResponse {
+  response: string;
+  success: boolean;
+}
+
+export const IAMessage = ({
+  response,
+  showDiv,
+  setShowDiv,
+  handleRequestIA,
+}: Props) => {
   const [customText, setCustomText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const isConditionallyRendered = response.response && showDiv;
+  useEffect(() => {
+    return () => {
+      setIsLoading(false);
+    };
+  }, [response]);
 
   const handleClick = () => {
     setShowDiv(!showDiv);
     setCustomText(getPhrase());
+  };
+  const handleReqIA = () => {
+    setIsLoading(true);
+    handleRequestIA();
   };
 
   return (
@@ -32,47 +55,78 @@ export const IAMessage = ({ response, isLoading }: Props) => {
         zIndex: 1000,
       }}
     >
-      {response.response && !isLoading ? (
-        <motion.div
-          className={`iabubble ${response.success ? "border-success" : "border-failure"}`}
-          initial={{ opacity: 0, scale: 0, transformOrigin: "top right" }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 180,
-            damping: 20,
-            mass: 1.1,
-            duration: 2,
-          }}
-        >
-          <div className="d-flex align-items-center gap-2">
-            <Image
-              src={`${!response.success ? "/mad_robot.png" : "/happy_robot.png"}`}
-              alt={"iaIcon"}
-              width={50}
-              height={50}
-              onClick={handleClick}
-              style={{ cursor: "pointer" }}
-            />
-            <h4>
-              {response.success ? "Resultado Correcto" : "Resultado Incorrecto"}
-            </h4>
-          </div>
-
-          <h6 className="iabubble-message pt-2">{response.response}</h6>
-        </motion.div>
-      ) : (
-        <>
-          <div className="spinner-container">
-            {isLoading && (
+      <div
+        style={{
+          width: "46vw",
+          height: "142px",
+        }}
+      >
+        {isConditionallyRendered ? (
+          <motion.div
+            className={`iabubble ${response.success ? "border-success" : "border-failure"}`}
+            initial={{ opacity: 0, scale: 0, transformOrigin: "top right" }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 180,
+              damping: 20,
+              mass: 1.1,
+              duration: 2,
+            }}
+          >
+            <div className="d-flex justify-content-between">
+              <div className="d-flex align-items-center gap-2">
+                <Image
+                  src={`${!response.success ? "/mad_robot.png" : "/happy_robot.png"}`}
+                  alt={"iaIcon"}
+                  width={50}
+                  height={50}
+                  onClick={handleClick}
+                  style={{ cursor: "pointer" }}
+                />
+                <h4>
+                  {response.success
+                    ? "Aqui tienes algunos tips:"
+                    : "Resultado Incorrecto"}
+                </h4>
+              </div>
+              <div>
+                <IoIosCloseCircleOutline
+                  size={35}
+                  className="close-button"
+                  onClick={() => setShowDiv(!showDiv)}
+                />
+              </div>
+            </div>
+            {isLoading ? (
               <div className="d-flex flex-column justify-content-center align-items-center">
-                <Spinner className="p-4" animation="grow" />
+                <Spinner className="p-3" animation="grow" />
                 <h6 className="mt-3">
-                  Verificando tu respuesta con Inteligencia Artificial
+                  Verificando tu respuesta... espera un momento
                 </h6>
               </div>
+            ) : (
+              <>
+                <h6 className="iabubble-message pt-2">{response.response}</h6>
+                {!response.success && (
+                  <div className="d-flex gap-3">
+                    <Button variant="primary" onClick={() => handleReqIA()}>
+                      Si
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setShowDiv(false)}
+                    >
+                      No
+                    </Button>
+                  </div>
+                )}
+              </>
             )}
-            {!isLoading && !response.response && showDiv && (
+          </motion.div>
+        ) : (
+          <>
+            {!response.response && showDiv && (
               <motion.div
                 className={`iabubble ${response.success ? "border-success" : "border-failure"}`}
                 initial={{ opacity: 0, scale: 0, transformOrigin: "top right" }}
@@ -108,9 +162,9 @@ export const IAMessage = ({ response, isLoading }: Props) => {
                 <h6 className="iabubble-message pt-2">{customText}</h6>
               </motion.div>
             )}
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
 
       <Image
         src={"/iaIcon.png"}
